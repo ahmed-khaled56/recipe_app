287b049a4a3566d6539c9dc75e131e711dd2c6b7
ahmed <mhmamdmf275@gmail.com>
2025-07-20 18:50:46 +0300

    show detailes of meal

--- lib/widgets/recipeDetailes.dart
+++ lib/widgets/recipeDetailes.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:recipes_app/model/recipe_model.dart';
 import 'package:recipes_app/widgets/ingredientRow.dart';
 
 class Recipedetailes extends StatelessWidget {
-  const Recipedetailes({super.key});
+  const Recipedetailes({super.key, required this.mealDetailes});
+
+  final DetailesModel mealDetailes;
 
   @override
   Widget build(BuildContext context) {
@@ -11,30 +14,48 @@ class Recipedetailes extends StatelessWidget {
       children: [
         ClipRRect(
           borderRadius: BorderRadius.circular(16),
-          child: Image.network(
-            'https://www.themealdb.com/images/media/meals/xvsurr1511719182.jpg',
-          ),
+          child: Image.network(mealDetailes.maleImage),
         ),
         const SizedBox(height: 12),
-        Text('EGYPT', style: const TextStyle(color: Colors.grey)),
+        Text(mealDetailes.area, style: const TextStyle(color: Colors.grey)),
         const SizedBox(height: 16),
         const Text(
           'Instructions',
           style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
         ),
         const SizedBox(height: 8),
-        Text(
-          "Preheat the oven to 200C/180C Fan/Gas 6. Put the biscuits in a large re-sealable freezer bag and bash with a rolling pin into fine crumbs",
-        ),
-        const SizedBox(height: 10),
+        Text(mealDetailes.descreption),
+        const SizedBox(height: 15),
 
         const Text(
           'Ingredients',
           style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
         ),
         const SizedBox(height: 8),
-        Ingredientrow(ingrediantname: "digestive biscuits", ingredientNum: 1),
-        Ingredientrow(ingrediantname: "butter", ingredientNum: 2),
+
+        // for (var ingredient in mealDetailes.ingerdiants)
+        //  for (var meseaur in mealDetailes.mesearus)
+
+        //     Ingredientrow(
+        //       ingrediantname: mealDetailes.ingerdiants[ingredient],
+        //       ingredientNum: ingredient,
+        //       meseaur:  mealDetailes.mesearus
+        //     ),
+        if (mealDetailes.ingerdiants != null && mealDetailes.mesearus != null)
+          ...List.generate(mealDetailes.ingerdiants.length, (i) {
+            final ingredient = mealDetailes.ingerdiants[i];
+            final measure = mealDetailes.mesearus[i];
+
+            if (ingredient != null && ingredient.trim().isNotEmpty) {
+              return Ingredientrow(
+                ingrediantname: ingredient.split(' ').take(2).join(' '),
+                ingredientNum: i + 1,
+                meseaur: measure ?? '',
+              );
+            } else {
+              return const SizedBox(); // عنصر فارغ
+            }
+          }),
       ],
     );
   }
