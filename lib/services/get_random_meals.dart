3e2998bdc12984215d001605726dc2f1a7f04572
ahmed <mhmamdmf275@gmail.com>
2025-07-22 20:32:22 +0300

    create Service to get random male

---
+++ lib/services/get_random_meals.dart
@@ -0,0 +1,16 @@
+import 'package:recipes_app/helper/API.dart';
+import 'package:recipes_app/model/recipe_model.dart';
+
+class GetRandomMeals {
+  Future<List<dynamic>> getRandomMeals({required String categoryName}) async {
+    Map<String, dynamic> randomMap = await API().get(
+      url: "https://themealdb.com/api/json/v1/1/search.php?f=c",
+    );
+
+    List<dynamic> RandomList = randomMap["meals"];
+
+    List<DetailesModel> randomMealsList =
+        RandomList.map((item) => DetailesModel.fromJson(item)).toList();
+    return randomMealsList;
+  }
+}
